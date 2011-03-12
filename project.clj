d5224c3e56dca2cd4b8cdd4ebb563b07e879cdfb
fuJiin <fu7iin@gmail.com>
2011-03-12 15:28:07 -0800

    add lein run as dev-dependencies - in case we need to use lein

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "TODO: add summary of your project"
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]
-                 [incanter "1.2.3"]])
+                 [incanter "1.2.3"]]
+  :dev-dependencies [[lein-run "1.0.1-SNAPSHOT"]])
