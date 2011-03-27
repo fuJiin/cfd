000b78d6937510ea0bd73dd75d304a70ed3ca4ad
fuJiin <fu7iin@gmail.com>
2011-03-27 14:29:11 -0700

    pass in original grid to properly calculate boundaries

--- src/CFD/odppde.clj
+++ src/CFD/odppde.clj
@@ -62,7 +62,7 @@
 (defn solve-with-tridiag
   "Solves implicit formula using tridiagonal matrix formulation.
    Takes in coefficents a, b, c, and a 1xn vector as RHS values for D"
-   [a b c d-grid]
+   [a b c d-grid orig-grid]
    (let [d-size (count d-grid)
          rhs    (remove nil?                            ;; filter out nils at beginning or end
                   (for [i (range 0 d-size)]
@@ -70,14 +70,18 @@
                       (or (= i 0) (= i (dec d-size)))   ;; return nil if at beginning or end
                         nil
                       (= i 1)                           ;; return D_n - a * u_1 for i = 2
-                        ($= (nth d-grid i) - a * (nth d-grid 0))
+                        ($= (nth d-grid i) - a * (nth orig-grid 0))
                       (= i (- d-size 2))                ;; return D_n - c * u_-1 for i = IM1
-                        ($= (nth d-grid i) - c * (nth d-grid (dec d-size)))
+                        ($= (nth d-grid i) - c * (nth orig-grid (dec d-size)))
                       :else                             ;; otherwise return point on grid
                         (nth d-grid i))))
          length (count rhs)
          coeffs (coeff-matrix length a b c)]            ;; get coefficient matrix
-      (mmult (solve coeffs) rhs)))                      ;; multiply inverse of coeff-matrix with rhs to solve grid
+      (vec
+        (flatten
+          (vector (nth orig-grid 0)                     ;; add initial boundary value with...
+                  (mmult (solve coeffs) rhs)            ;; multiply inverse of coeff-matrix with rhs to solve grid
+                  (nth orig-grid (dec d-size)))))))     ;; and end boundary value
 
 (defn laasonen
   "Uses Laasonen implicit method to solve n+1.
@@ -86,7 +90,7 @@
   (let [coeff   ($= alpha * t-step / (x-step ** 2))
         b       (- ($= 2 * coeff + 1))
         d-grid  (map #(- %) grid)] ;; RHS is negative of current point for Laasonen
-    (solve-with-tridiag coeff b coeff d-grid)))
+    (solve-with-tridiag coeff b coeff d-grid grid)))
 
 (defn crank-nicolson
   "Uses Crank-Nicolson to solve n+1"
@@ -103,4 +107,4 @@
                                                /
                                                ($= x-step ** 2))))))
                       grid)]                    ;; apply map function to current grid
-    (solve-with-tridiag coeff b coeff d-grid)))
+    (solve-with-tridiag coeff b coeff d-grid grid)))
