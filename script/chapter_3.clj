77053ad4c6641c8745ee14a0a18d7c71c98c8dc1
fuJiin <fu7iin@gmail.com>
2011-04-06 22:41:54 -0700

    fix analytical solver in chapter 3

--- script/chapter_3.clj
+++ script/chapter_3.clj
@@ -1,17 +1,19 @@
 (ns cfd.script.chapter-3
   (:use [incanter.core])
   (:use [cfd.odppde]))
-  
-(defn analytic
-  [ti ts l x n alpha t]
+
+(defn analytic-solve
+  "Analytical solution for 3.1"
+  [ts ti l x t alpha n]
   (let [f (fn [m]
             ($= (Math/E ** (0 - (((m * Math/PI / l) ** 2) * alpha * t)))
                 *
                 (1 - (-1 ** m))
                 *
-                (sin (m * Math/PI * x / l))
+                (sin ($= m * Math/PI * x / l))
                 /
                 (m * Math/PI)))
-        series (take n
-                (map f (iterate inc 1)))]
-    ($= ti + 2 * (ti - ts) * series)))
+        series (reduce +
+                (take n
+                  (pmap f (iterate inc 1))))]
+    ($= ts + 2 * (ti - ts) * series)))
