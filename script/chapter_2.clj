02d3e20098d0c45da76b0b6a23e9749af9fdb1db
fuJiin <fu7iin@gmail.com>
2011-04-21 20:04:51 -0700

    2.12
    - function to calculate and save for all scenarios
    - comments

--- script/chapter_2.clj
+++ script/chapter_2.clj
@@ -1,41 +1,41 @@
 (ns cfd.script.chapter-2
   (:use [cfd.fd-approx])
-  (:use [incanter.core]))
+  (:use [incanter core io]))
 
+;; Problem Conditions ;;
 (defn f [x]
   ($= x ** 3 - 5 * x))
-  
-(defn f-deriv [x]
+
+(defn f-deriv [x]   ; first derivative
   ($= 3 * (x ** 2) - 5))
 
-(defn f-deriv-2 [x]
+(defn f-deriv-2 [x] ; second derivative
   ($= 6 * x))
-  
-(def i-vals [0.5 1.5])  
-  
+
+(def i-vals [0.5 1.5])
+
 (def step-sizes [0.00001 0.0001 0.001 0.01 0.1 0.2 0.3])
 
-(defn get-approx
-  "Creates a table for specified finite difference approximation.
-   Takes in power, order, function, x, and steps to iterate through.
-   It'll find all methods available for the order, and format results
-   for each method in a separate row."
-  [power order func x steps]
-  (let [c-map   (get-coeff-map order)               ;; get coefficients
-        data    (map (fn [method]                   ;; get rows of data
-                       (cons (name method)          ;; ...made of computation done with each step 
-                             (map #(fd-approx power order method func x %) steps)))
-                     (keys c-map))                  ;; ...for each differencing method
-        header  (cons "Differencing" step-sizes)]   ;; table headers
-    (dataset header data)))                         ;; generate table set
-    
-(defn approx-compare
-  "Runs approximations and compare to actual values"
-  [power order func x steps]
-  (let [approx  (get-approx power order func x steps) ;; get approximations
-        deriv   (case power                           ;; figure out which derivative to use
-                  1   f-deriv                         ;; for actual calcalation
-                  2   f-deriv-2)
-        actual  (if deriv (deriv x) nil)]             ;; calculate actual value
-    {:actual actual                                   ;; map results for comparison
-     :approx approx}))
+(def meths [:central :backward :forward])
+
+;; Functions to calculate aggregate data for 2.12 ;;
+(defn calc-error
+  "Calculates percentage error"
+  [actual expected]
+  ($= 100 * (actual - expected) / expected))
+
+(defn run
+  "Runs through all scenarios for problem 2.12.
+   Splits up data by x-value and method used,
+   aggregating approximations for all step sizes."
+  []
+  (doseq [x i-vals]   ; iterate through x values (0.5 1.5)
+    (doseq [m meths]  ; iterate through methods (central, backward, forward)
+      (let [title (str x "-" (name m))
+            data  (for [step step-sizes]                              ; iterate through x-steps
+                    (let [y       (fd-approx 2 2 m f x step)          ; compute at x with given method, step-size
+                          error   (abs (calc-error y (f-deriv-2 x)))] ; compute error
+                       {:step-size  step
+                       :value       y
+                       :error       error}))]
+        (save (to-dataset data) (str "/users/jadis/desktop/" title ".csv"))))))
