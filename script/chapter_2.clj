4a570b36c9b2a3e7360b0e4756ad9482a96e04f4
fuJiin <fu7iin@gmail.com>
2011-04-22 03:15:49 -0700

    vary power too

--- script/chapter_2.clj
+++ script/chapter_2.clj
@@ -29,13 +29,20 @@
    Splits up data by x-value and method used,
    aggregating approximations for all step sizes."
   []
-  (doseq [x i-vals]   ; iterate through x values (0.5 1.5)
-    (doseq [m meths]  ; iterate through methods (central, backward, forward)
-      (let [title (str x "-" (name m))
-            data  (for [step step-sizes]                              ; iterate through x-steps
-                    (let [y       (fd-approx 2 2 m f x step)          ; compute at x with given method, step-size
-                          error   (abs (calc-error y (f-deriv-2 x)))] ; compute error
-                       {:step-size  step
-                       :value       y
-                       :error       error}))]
-        (save (to-dataset data) (str "/users/jadis/documents/cfd/chapter-2/" title ".csv"))))))
+  (doseq [power [1 2]]
+    (let [title-power (case power     ; set power component of title
+                        1 "1st-deriv"
+                        2 "2nd-deriv")
+          deriv-func  (case power     ; set derivative function based on power
+                        1 f-deriv
+                        2 f-deriv-2)]
+    (doseq [x i-vals]   ; iterate through x values (0.5 1.5)
+      (doseq [m meths]  ; iterate through methods (central, backward, forward)
+        (let [title (str x "-" (name m) "-" title-power)
+              data  (for [step step-sizes]                                ; iterate through x-steps
+                      (let [y       (fd-approx power 2 m f x step)        ; compute at x with given method, step-size
+                            error   (abs (calc-error y (deriv-func x)))]  ; compute error
+                         {:step-size  step
+                         :value       y
+                         :error       error}))]
+          (save (to-dataset data) (str "/users/jadis/documents/cfd/chapter-2/" title ".csv"))))))))
